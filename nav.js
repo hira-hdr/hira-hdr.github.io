879c1a86231a97f7c41e069c9ecb35a1ccde1bd0
hira-hdr <dahalhiranath@gmail.com>
2024-08-20 08:52:29 +1000

    Update nav.js
    Udpates in JavaScript

--- nav.js
+++ nav.js
@@ -1,5 +1,3 @@
-//creates an alert 
-alert('Hello World')
 /*Toggle between adding and removing the 'responsive' class to topnav when the user clicks on the icon*/
 function openMenu(){
     var x = document.getElementById("portfolioTopnav");
